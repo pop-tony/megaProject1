e729cb7a9e76d672547559a1eb481b6e9cc9eaf1
tony <poptonydm@gmail.com>
2025-09-05 08:14:37 +0000

    moved to https

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [
@@ -7,5 +8,16 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    proxy: {
+      '/api': {
+        target: 'https://192.168.1.169:8443',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
